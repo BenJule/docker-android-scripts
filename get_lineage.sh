3e8e6c08bdae19683b18a8c6022597a7ac5e234d
BenJule <BenJule@users.noreply.github.com>
2017-09-03 22:42:49 +0200

    Update get_lineage.sh

--- get_lineage.sh
+++ get_lineage.sh
@@ -2,8 +2,8 @@
 curl https://storage.googleapis.com/git-repo-downloads/repo > repo
 chmod a+x repo
 export PATH=$PWD:$PATH
-git config --global user.name "Varun Bhat"
-git config --global user.email "varunbhat.kn@gmail.com"
+git config --global user.name "BenJule"
+git config --global user.email "benlue@s3root.ovh"
 
 repo init -u https://github.com/LineageOS/android.git -b cm-14.1
 repo sync 
